ca325e7488130c4a8b409a153dcd3b039387a269
LaithH10 <laiethhadid172@gmail.com>
2025-02-11 19:55:32 +0100

    Routes

--- searchEngine/src/Pages/Home.jsx
+++ searchEngine/src/Pages/Home.jsx
@@ -1,13 +1,21 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState ("");
+
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "John Wick", release_date: "2022" },
     { id: 3, title: "John Wick", release_date: "2024" },
   ];
 
-  const handelSearch = () => {};
+  const handelSearch = (e) => {
+    e.preventDefault()
+    alert(searchQuery)
+    setSearchQuery("")
+  };
 
   return (
     <div className="home">
@@ -16,6 +24,8 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">Search</button>
       </form>
